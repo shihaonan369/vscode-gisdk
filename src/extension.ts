967a5d0178b74bbce9a9dc922c0a834c448556a9
unknown <Administrator@KEULVZ8SHKWRM6T.transcad.tpri.org.cn>
2020-08-04 18:29:48 +0800

    support compiler selection

--- src/extension.ts
+++ src/extension.ts
@@ -5,28 +5,72 @@ import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-gisdk" is now active!');
 
+	// Get the path of all compiler installed in system.
+	const utils = require('./utils.js');
+	const compilers : string[] = await utils.getCompilersPath();
+
+	// Create a select bar to select which rscc compiler will be used.
+	let rsccSelectBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 11);
+	rsccSelectBar.command = "vscode-gisdk.selectCompiler";
+	rsccSelectBar.tooltip = "Select which compiler you want to use.";
+
+	let compilerState = context.globalState.get<string>('compilerPath');
+	if (compilerState) {
+		rsccSelectBar.text = '$(codicon-settings)' + 'GISDK' + '(' + path.dirname(compilerState) + '\\' + path.basename(compilerState) + ')';
+	} else if (compilers) {
+		context.globalState.update('compilerPath', compilers[0]);
+		rsccSelectBar.text = '$(codicon-settings)' + 'GISDK' + '(' + path.dirname(compilers[0]) + '\\' + path.basename(compilers[0]) + ')';
+	} else {
+		rsccSelectBar.text = '$(codicon-settings)' + 'There\'s no rscc installed.';
+	}
+	
+	vscode.window.activeTextEditor?.document.languageId !== 'gisdk' || rsccSelectBar.show();
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((e) => {
+		if (e?.document.languageId === 'gisdk') {
+			rsccSelectBar.show();
+		} else {
+			rsccSelectBar.hide();
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("vscode-gisdk.selectCompiler", () => {
+		vscode.window.showQuickPick(compilers, { canPickMany: false }).then((picked) => {
+			if (picked) {
+				context.globalState.update('compilerPath', picked);
+				rsccSelectBar.text = '$(codicon-settings)' + 'GISDK' + '(' + path.dirname(picked) + '\\' + path.basename(picked) + ')';
+			}
+		});
+	}));
+	
+
 	context.subscriptions.push(vscode.tasks.registerTaskProvider("rscc", {
 		async provideTasks() {
+			// Check if has any avaliable compiler.
+			let compilerPath = context.globalState.get<string>('compilerPath');
+			if (!compilerPath) {
+				return undefined;
+			}
+
 			// check if there is any file opend
 			let file = vscode.window.activeTextEditor?.document.uri.fsPath;
 			if (!file) {
 				return undefined;
 			}
 
-			// get path of compiler from registry
-			const utils = require('./utils.js');
-			let compilerPath = await utils.getCompilerPath();
+			let extName = path.extname(file);
+			if (extName.match(/\.rsc|\.gisdk|\.model|\.scenarios/g)?.length === 0) {
+				return undefined;
+			}
 
 			// compile the current opend file by setting difinition
-			let definition : vscode.TaskDefinition = {type: 'rscc'};
+			let definition: vscode.TaskDefinition = { type: 'rscc' };
 			let fileDirName = path.dirname(file);
-			let extName = path.extname(file);
 			let fileName = path.basename(file, extName);
 			definition.args = [
 				"-c",
@@ -46,19 +90,19 @@ export function activate(context: vscode.ExtensionContext) {
 		async resolveTask(_task: vscode.Task) {
 			// get path of compiler from registry
 			const utils = require('./utils.js');
-			let compilerPath = await utils.getCompilerPath();
+			let compilerPath = context.globalState.get<string>('compilerPath') ?? _task.definition.compilerPath;
 
 			let defaultShell = vscode.workspace.getConfiguration('terminal').get<string>('integrated.shell.windows');
 			let len = defaultShell?.match(/powershell\.exe/)?.length;
 			if (len && len > 0) {
-				return new vscode.Task(_task.definition, _task.name, _task.source,  new vscode.ShellExecution("& '" + compilerPath + "' " + _task.definition['args'].toString().replace(/,/g, ' ')), "$rscc");
+				return new vscode.Task(_task.definition, _task.name, _task.source, new vscode.ShellExecution("& '" + compilerPath + "' " + _task.definition['args'].toString().replace(/,/g, ' ')), "$rscc");
 			} else {
 				return new vscode.Task(_task.definition, _task.name, _task.source, new vscode.ShellExecution('"' + compilerPath + '" ' + _task.definition['args'].toString().replace(/,/g, ' ')), "$rscc");
 			}
 		}
 	}));
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('gisdk', {
-		provideDebugConfigurations(folder: vscode.WorkspaceFolder | undefined) : vscode.ProviderResult<vscode.DebugConfiguration[]> {
+		provideDebugConfigurations(folder: vscode.WorkspaceFolder | undefined): vscode.ProviderResult<vscode.DebugConfiguration[]> {
 			return [
 				{
 					type: 'gisdk',
@@ -71,4 +115,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('......');
+}
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,35 @@
-exports.getCompilerPath = function () {
+const regedit = require('regedit');
+const path = require('path');
+exports.getCompilersPath = function () {
+    return new Promise(async (resolve, reject) => {
+        let compilersPath = [];
+        let caliper = 'HKLM\\SOFTWARE\\Caliper Corporation';
+        let result = await regeditList(caliper);
+        for (let i = 0; i < result[caliper].keys.length; i++) {
+            let software = caliper + '\\' + result[caliper].keys[i];
+            let result1 = await regeditList(software);
+            for (let j = 0; j < result1[software].keys.length; j++) {
+                let version = software + '\\' + result1[software].keys[j];
+                let result2 = await regeditList([version]);
+                compilersPath.push(path.normalize(result2[version].values['Installed In'].value + '\\' + 'rscc.exe'));
+            }
+        }
+        if (compilersPath.length > 0) {
+            resolve(compilersPath);
+        } else {
+            resolve(undefined);
+        }
+    });
+};
+
+function regeditList(keys) {
     return new Promise((resolve, reject) => {
-        let regedit = require('regedit');
-        let installedIn = "";
-        regedit.list(['HKLM\\SOFTWARE\\Caliper Corporation\\TransCAD'], (err, result) => {
+        regedit.list(keys, (err, result) => {
             if (err) {
                 reject();
             } else {
-                if (result['HKLM\\SOFTWARE\\Caliper Corporation\\TransCAD'].keys.length > 0) {
-                    let key = 'HKLM\\SOFTWARE\\Caliper Corporation\\TransCAD' + '\\' + result['HKLM\\SOFTWARE\\Caliper Corporation\\TransCAD'].keys[0];
-                    regedit.list([key], (err1, result1) => {
-                        if (err1) {
-                            reject();
-                        } else {
-                            installedIn = result1[key].values['Installed In'].value + 'rscc.exe';
-                            resolve(installedIn);
-                        }
-                    });
-                }
+                resolve(result);
             }
         });
     });
-};
+}
